bb941a83d0a2118667e79b4b40525409e7a9f14d
juliocabrera <juliocabrera820@gmail.com>
2019-11-07 12:34:59 -0600

    Operadores: map,filter,take.takeLast

--- src/main/java/operators/Operadores.java
+++ src/main/java/operators/Operadores.java
@@ -4,9 +4,20 @@ import io.reactivex.Observable;
 
 public class Operadores {
     public static void main(String[] args) {
-        Integer[] numbers = {10,8,22,11,-10,3,21,7,9,18,12,18,10};
+        Integer[] numbers = {10,11,8,22,11,-10,3,21,7,9,18,12,18,18,14,13,20};
 
         Observable.fromArray(numbers).map(x -> "["+x+"] ")
-                                     .subscribe(System.out::print);
+                                     .subscribe(x -> System.out.print(x+" "));
+        System.out.println("\n"+"Numeros pares");
+        Observable.fromArray(numbers).filter(x -> x%2==0)
+                                     .subscribe(x -> System.out.print(x+" "));
+        System.out.println("\n"+"Toma los 4 ultimos numeros pares");
+        Observable.fromArray(numbers).filter(x -> x%2==0)
+                                     .takeLast(4)
+                                     .subscribe(System.out::println);
+        System.out.println("\n"+"Toma los primeros 2 numeros pares");
+        Observable.fromArray(numbers).filter(x -> x%2==0)
+            .take(2)
+            .subscribe(System.out::println);
     }
 }
